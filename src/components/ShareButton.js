be919e5d3d4cfaaee5aab6d140d6780c7707fbbe
Fernando <valanife@gmail.com>
2022-10-06 01:04:38 -0300

    test

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -23,6 +23,7 @@ async function testWebShare({ text, files: filesUrl }) {
   // Handle file urls
   if (filesUrl && filesUrl.length > 0) {
     const filesGetter = filesUrl.map((file) => urlToObject(file));
+    alert(filesGetter);
     const newFiles = await Promise.all(filesGetter);
 
     if (!navigator.canShare || !navigator.canShare({ files: newFiles })) {
